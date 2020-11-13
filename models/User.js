7d3f01f8081531cea53d6a0ed19296ebb1e96abc
tinglewis633 <72407518+tinglewis633@users.noreply.github.com>
2020-11-13 16:38:44 -0500

    Set up MongoDB and some routes

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("moongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  avatar: {
+    type: String,
+  },
+
+  dateJoined: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
+//Connect Database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
