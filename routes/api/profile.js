327d8d7b50b27be9446cafad5e3390e78afbe999
tinglewis633 <72407518+tinglewis633@users.noreply.github.com>
2020-11-18 17:39:50 -0500

    More routes
    delete user and his/her profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -136,4 +136,20 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route     DELETE api/profile
+// @desc      Delete profile user & posts
+// @access    Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    //todo - remove user post
+    // remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "user removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
