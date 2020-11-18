96ce7dda4fe7eedcddc2840aba4b8ef726271896
tinglewis633 <72407518+tinglewis633@users.noreply.github.com>
2020-11-18 17:19:23 -0500

    More routes
    get all profiles,
    get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -99,9 +99,41 @@ router.post(
       return res.json(profile);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send("Server Error");
     }
   }
 );
+
+// @route     GET api/profile
+// @desc      Get all profile
+// @access    Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get profile by user ID
+// @access    Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "profile not found" });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if ((err.kind = "ObjectId")) {
+      return res.status(400).json({ msg: "profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
