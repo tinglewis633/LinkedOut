c620dd60d5051df39a80e6bc629a9313afdf2e4f
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-01-20 14:01:42 -0500

    Setup state for login&register

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,85 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log("success");
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>Create Your Account
+      </p>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              minlength='6'
+              name='password'
+              value={password}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              minlength='6'
+              name='password2'
+              value={password2}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+        </div>
+        <input type='submit' value='Register' className='btn btn-primary' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Sign in</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
