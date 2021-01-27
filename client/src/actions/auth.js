1ae68a77a6263b99aa075ea3e76b6e87db439dc8
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-01-27 11:39:50 -0500

    createPorfile
    connect front-end to back-end

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 //Load User
@@ -94,6 +95,9 @@ export const login = (email, password) => async (dispatch) => {
 
 //Logout / Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
