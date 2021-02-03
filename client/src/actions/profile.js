50cf74c0c155b73d629d6bb5a7ee1fbd60408d64
Lewis <72407518+tinglewis633@users.noreply.github.com>
2021-02-03 17:33:32 -0500

    Profiles' Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -46,7 +46,7 @@ export const getProfiles = () => async (dispatch) => {
 };
 
 // Get all profiles
-export const getProfileByID = (userId) => async (dispatch) => {
+export const getProfileById = (userId) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
     dispatch({
